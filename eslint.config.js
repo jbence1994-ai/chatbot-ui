8f3ea1a6953c0471214b746b037c1e06418a167d
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-26 13:37:41 +0200

    Install eslint-plugin-simple-import-sort and define a base importing order

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -18,6 +19,37 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1) React & frameworks
+            ['^react', '^next'],
+
+            // 2) Packages (node_modules)
+            ['^@?\\w'],
+
+            // 3) Absolute aliases (e.g. @ or src/)
+            ['^(@|src)(/.*|$)'],
+
+            // 4) Side-effect imports (e.g. polyfills, CSS resets)
+            ['^\\u0000'],
+
+            // 5) Relative imports
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            // 6) Styles
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
     },
   },
 ]);
